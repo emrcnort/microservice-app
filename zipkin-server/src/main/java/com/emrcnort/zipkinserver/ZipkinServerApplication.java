631d7b61901c93b9dc24cabeb88de77b75cca05f
Emrecan Ortakcı <emrcn.ort@hotmail.com>
2022-07-12 18:43:37 +0300

    zipkin server and logging

---
+++ zipkin-server/src/main/java/com/emrcnort/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package com.emrcnort.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class,args);
+    }
+}
